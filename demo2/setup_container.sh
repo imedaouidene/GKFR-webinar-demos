07d36e6767b29a879ef3462c623c62f73e4658cb
root <root@dns.linsoft.int>
2021-04-14 13:16:59 +0100

    adding demo3 files

---
+++ demo2/setup_container.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# setup container web app
+
+# create container image with Dockerfile
+podman build -t myweb:v1 -f ./Dockerfile
+
+# run container with container image
+podman run -d --name mywebcontainer -p 8080:80 myweb:v1
+
+# check web page from local machine
+curl http://$(podman inspect mywebcontainer --format "{{ .NetworkSettings.IPAddress }}")
+
+# open firewall
+firewall-cmd --add-port=8080/tcp --permanent && firewall-cmd --reload
+
+
